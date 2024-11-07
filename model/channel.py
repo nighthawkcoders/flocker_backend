a80cdbddcaa5a8d8e68de332c5a39f93638ca0ab
Darsh220 <darsh.srivastava@gmail.com>
2024-11-07 10:43:37 -0800

    ds

--- model/channel.py
+++ model/channel.py
@@ -185,8 +185,7 @@ def initChannels():
         # P2 channels below:
         culinary_posts = Group.query.filter_by(_name='Culinary Posts').first()
         create_and_compete_channels = [
-            Channel(name='Recipes', group_id=culinary_posts.id),
-            Channel(name='Ingredients', group_id=culinary_posts.id),
+            
         ]
         # P3 channels below:
         
